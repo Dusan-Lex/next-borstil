ece65d624906fd1eac4da566547c890d13c1ba4b
Dusan-Lex <dusanlexmilosavljevic@gmail.com>
2021-12-07 01:48:34 +0100

    fixed sidebar

--- components/Navigation/Sidebar/SidebarItem/SidebarItemStyles.js
+++ components/Navigation/Sidebar/SidebarItem/SidebarItemStyles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { color } from "../../../../shared/utils/styles";
 
 export const ItemBox = styled.li`
+  min-height: 5.3rem;
   overflow: hidden;
 `;
 
